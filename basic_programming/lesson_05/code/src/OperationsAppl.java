194009e71737d191a5364a50dbfad2cdaee69ce7
edward <edward@edward>
2023-09-11 14:29:14 +0300

    Lesson 5, code and task

---
+++ basic_programming/lesson_05/code/src/OperationsAppl.java
@@ -0,0 +1,31 @@
+public class OperationsAppl {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 10;
+        int z = x + y;
+        System.out.println(z);
+        z = x - y;
+        System.out.println(z);
+        z = x * y;
+        System.out.println(z);
+        z = x / y;
+        System.out.println(z);
+        z = y / x;
+        System.out.println(z);
+        //3.55 * 10 = 35.5
+        double a =  y / (x * 1.);
+        System.out.println(a);
+        a = (x + y) / 10;
+        System.out.println(a);
+        z = (int) (18. / 7);
+        System.out.println(z);
+        z = 18 % 7;
+        System.out.println(z);
+        a = 18. / 7;
+        System.out.println(a);
+        z = 17 % 5;
+        System.out.println(z);
+        z = 3 % 5;
+        System.out.println(z);
+    }
+}
--- basic_programming/lesson_05/homework.md
+++ basic_programming/lesson_05/homework.md
@@ -2,6 +2,10 @@
 
 **Task 1**
 
+In the MethodAdvAppl class, create a method that takes a radius and calculates the area of a circle. In the main method, check the work of the method by calling it several times. Print the result to the console.
+
 
 **Задача 1**
 
+В классе MethodAdvAppl создать метод принимающий радиус и вычисляющий площадь окружности.
+В методе main проверить работу метода вызвав его несколько раз. Результат вывестив консоль.
