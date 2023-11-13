8949a89273eae49be772e369046cc734d5d2f143
edward <edward@pop-os.localdomain>
2023-11-13 14:18:44 +0200

    Lesson 46, code and task

---
+++ basic_programming/lesson_46/code/src/ListAppl.java
@@ -0,0 +1,54 @@
+package ait.list;
+
+import ait.list.interfaces.IList;
+import ait.list.model.MyLinkedList;
+
+import java.util.Iterator;
+
+public class ListAppl {
+    public static void main(String[] args) {
+        IList<Integer> list = new MyLinkedList<>();
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        list.add(2);
+        list.add(7);
+        list.add(5);
+        list.add(3);
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        list.add(7);
+        list.add(null);
+        System.out.println(list.size());
+        System.out.println(list.get(2));
+        System.out.println(list.get(5));
+        System.out.println(list.indexOf(3));
+        System.out.println(list.indexOf(7));
+        System.out.println(list.indexOf(null));
+        System.out.println(list.indexOf(10));
+        Integer num = list.remove(5);
+        System.out.println(num);
+        System.out.println(list.size());
+        System.out.println(list.remove((Integer) 7));
+        System.out.println(list.size());
+//        System.out.println("===== Homework =====");
+//        list.add(4, 13);
+//        System.out.println(list.size());
+//        System.out.println(list.get(4));
+//        for (Integer n : list) {
+//            System.out.println(n);
+//        }
+//        list.clear();
+//        System.out.println(list.isEmpty());
+//        System.out.println("===== List of String =====");
+//        IList<String> myList = new MyLinkedList<>();
+//        myList.add("Boston");
+//        myList.add("Atlanta");
+//        myList.add("Chicago");
+//        myList.add("Boston");
+//        myList.add("New York");
+//        Iterator<String> iterator = myList.iterator();
+//        while(iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+    }
+}
--- basic_programming/lesson_46/homework.md
+++ basic_programming/lesson_46/homework.md
@@ -4,18 +4,18 @@
 
 **Task 1**
 
-Finish implementing the methods of the MyArrayList class. When deleting an element from MyArrayList, there is no need to reduce the elements array.  <br/>
+Finish implementing the methods of the MyLinkedList class.  <br/>
 
 **Task 2**
 
-Above each method of the MyArrayList class, indicate in the comments the computational complexity of this method.  <br/>
+Above each method of the MyLinkedList class, indicate in the comments the computational complexity of this method.  <br/>
 
 <br/>
 
 **Задача 1**
 
-Завершить реализацию методов класса MyArrayList. При удалении элемента из MyArrayList, сокращать массив elements не надо.  <br/>
+Завершить реализацию методов класса MyLinkedList.  <br/>
 
 **Задача 2**
 
-Над каждым из методов класса MyArrayList в комментариях указать вычислительную сложность этого метода.  <br/>
+Над каждым из методов класса MyLinkedList в комментариях указать вычислительную сложность этого метода.  <br/>
